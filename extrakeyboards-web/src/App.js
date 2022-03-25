e118e81a292573c05ffde35e0bd057f3cd4beb6f
Bo Dong <41502db@gmail.com>
2022-03-25 15:30:31 -0700

    formatDetermination
    1st format following twitch project

---
+++ extrakeyboards-web/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Header, Content, Sider } = Layout;
+
+class App extends React.Component {
+  render = () => (
+    <Layout>
+      <Header>{"Header"}</Header>
+      <Layout>
+        <Sider width={300} className="site-layout-background">
+          {"Sider"}
+        </Sider>
+        <Layout style={{ padding: "24px" }}>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              height: 800,
+              overflow: "auto",
+            }}
+          >
+            {"Home"}
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default App;
